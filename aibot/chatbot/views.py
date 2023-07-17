f5fe4ad1a5cb12cf635465a9581eff6ebd1facac
Hamoozi <moguristhebest@gmail.com>
2023-07-17 17:45:07 -0400

    feat: webpage sends response

--- aibot/chatbot/views.py
+++ aibot/chatbot/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 def chatbot(request):
     if request.method == "POST":
-        pass
+        message = request.POST.get('message')
+        #This will be replaced by api responce in the future
+        response = "Response"
+        return JsonResponse({'message' : message, 'response' : response})
     return render(request, 'chatbot.html')
