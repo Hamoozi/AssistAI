e29a6d7840c523ebdbd5ce48ce9c39d598c6e78f
Hamoozi <moguristhebest@gmail.com>
2023-07-17 18:02:03 -0400

    fix: user can now send and get response on page

--- aibot/chatbot/views.py
+++ aibot/chatbot/views.py
@@ -3,9 +3,9 @@ from django.http import JsonResponse
 
 # Create your views here.
 def chatbot(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         message = request.POST.get('message')
         #This will be replaced by api responce in the future
-        response = "Response"
-        return JsonResponse({'message' : message, 'response' : response})
+        response = 'I\'m doing great! How about you?'
+        return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html')
