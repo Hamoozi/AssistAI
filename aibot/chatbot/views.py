a7ca7b476281936a14782960741fea0b466568c8
Hamoozi <moguristhebest@gmail.com>
2023-08-07 00:42:38 -0400

    feat: openai response generation

--- aibot/chatbot/views.py
+++ aibot/chatbot/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import openai
 
 # Create your views here.
+
+def ask_openai(message):
+    response = openai.Completion.create(
+        model = "text-davinci-003"
+        prompt = message
+        max_tokens=150,
+        stop = None,
+        temperature=0.7,
+
+
+    )
+    print(response)
+    answer = response.choice[0].text.strip()
+
+
 def chatbot(request):
     if request.method == 'POST':
         message = request.POST.get('message')
